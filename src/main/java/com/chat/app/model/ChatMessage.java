ba23a1b06011f3940de7ad894832dbfbe12e1bb8
aryansenpaii <aryansenpai256@gmail.com>
2026-02-07 15:16:51 +0530

    First commit 🚀🚀🚀

---
+++ src/main/java/com/chat/app/model/ChatMessage.java
@@ -0,0 +1,27 @@
+package com.chat.app.model;
+
+public class ChatMessage {
+
+    private String sender;
+    private String content;
+
+    public ChatMessage() {
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
+
